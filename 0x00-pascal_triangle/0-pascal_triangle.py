d8df2558832a64fbefdeb47a9b33f0c72c724aeb
Gamaliel Adun <adungamaliel@gmail.com>
2023-09-27 09:59:25 +0100

    REPO init

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""
+Pascal's Triangle
+"""
+
+def pascal_triangle(n):
+    """
+    Function that returns list of lists of integers
+    representing the Pascal's triangle
+
+    Args:
+      n: number of levels
+    Returns:integers
+      list of lists of 
+    """
+
+    while n:
+        vagrant 
