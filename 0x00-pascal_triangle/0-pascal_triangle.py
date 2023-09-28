286d98e2860e8b16229dd1e27797052e139a316f
Gamaliel Adun <adungamaliel@gmail.com>
2023-09-28 10:50:22 +0100

    added 1

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -11,9 +11,20 @@ def pascal_triangle(n):
 
     Args:
       n: number of levels
-    Returns:integers
-      list of lists of 
+    Returns:
+      integers: pascal's triangle
     """
 
-    while n:
-        vagrant 
+    if n <= 0:
+        return []
+    final_list = []
+    curr_list = [1]
+
+    while n > 0:
+        final_list.append(curr_list)
+        curr_list = [0] + curr_list + [0]
+        curr_list = [(curr_list[i] + curr_list[i+1]) for i in range(len(curr_list) - 1)]
+        n-=1
+        if n == 0:
+            break
+    return final_list
